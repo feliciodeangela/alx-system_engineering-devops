6f394f3b9f6b2797fd0a845753550ef8ae29d821
feliciodeangela <feliciodeangela@gmail.com>
2023-11-25 10:45:16 +0200

    up task 1 packaging correction

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,11 @@
 # Install flask using pip3
 
-exec { 'flask installation':
-  command => '/usr/bin/pip3 install flask==2.1.0',
+package { 'werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3'
+}
+
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3'
 }
