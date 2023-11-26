8a522caafeedfb53105de30f95c2f02fe6f08995
feliciodeangela <feliciodeangela@gmail.com>
2023-11-26 18:32:13 +0200

    up task 100 try 2

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,6 +1,11 @@
-exec { 'Turn off passwd auth':
-  command => '/usr/bin/echo "PasswordAthentication no" >> /etc/ssh/ssh_config'
+file { '/etc/ssh/ssh_config':
+  ensure  => 'present',
+  content => 'PasswordAthentication no'
 }
+# exec { 'Turn off passwd auth':
+#   command => '/usr/bin/echo "PasswordAthentication no" >> /etc/ssh/ssh_config'
+# }
 exec { 'Declare identity file':
-  command => '/usr/bin/echo "IdentityFile ~/.ssh/school" >> /etc/ssh/ssh_config'
+  command => '/usr/bin/echo "IdentityFile ~/.ssh/school" >> /etc/ssh/ssh_config',
+  require => File['/etc/ssh/ssh_config']
 }
