b0fbcdf60e92b4ceb42fc68ab8fa564e5fd7e989
feliciodeangela <feliciodeangela@gmail.com>
2023-11-26 14:53:06 +0200

    up task 100

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,8 @@
+file { 'Turn off passwd auth':
+  path    => '/etc/ssh/config',
+  content => 'PasswordAthentication no'
+}
+file { 'Declare identity file':
+  path => '/etc/ssh/config',
+  content => 'IdentityFile ~/.ssh/school'
+}
